7bbb2c09c9aea4b58082bc035d9686287f2d9a6d
Subham Saha <subhamsahadeep@gmail.com>
2020-07-16 01:13:03 +0530

    List of Colleges along with integration done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-
+import * as data from '../assets/colleges.json';
+import { Colleges } from './models/colleges.model.js';
+  
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +9,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'hello-Angular';
+  colleges: Colleges = (data as any).colleges;
+  constructor(){}
+  ngOnInit(){
+    console.log(this.colleges);
+  }
 }
